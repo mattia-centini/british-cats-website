9153313ef932619e13332f8e7196ad0bae7941b2
Mattia Centini <mattia.centini@gmail.com>
2020-07-28 17:17:11 +0200

    mobile version terminated

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-// defining global variabled
-const menu = document.querySelector(".menu");
-const outerOne = document.querySelector(".outerOne");
-const outerTwo = document.querySelector(".outerTwo");
-const checkBox = document.querySelector(".toggler");
-const liItems = document.querySelectorAll(".outerTwo ul li");
-const section = [...document.querySelectorAll("section")];
+document.addEventListener("DOMContentLoaded", () => {
+  // defining global variabled
+  const menu = document.querySelector(".menu");
+  const outerOne = document.querySelector(".outerOne");
+  const outerTwo = document.querySelector(".outerTwo");
+  const checkBox = document.querySelector(".toggler");
+  const liItems = document.querySelectorAll(".outerTwo ul li");
+  const section = [...document.querySelectorAll("section")];
 
-//burger menu pop-up
-checkBox.addEventListener("click", () => {
-  if (menu.style.visibility === "hidden") {
-    menu.style.visibility = "visible";
-    outerOne.style.transform = "scale(1)";
-    outerOne.style.transitionDuration = `${0.75}s`;
-    outerTwo.style.opacity = 1;
-    console.log("clicked");
-  } else {
-    menu.style.visibility = "hidden";
-    outerOne.style.transform = "scale(0)";
-    outerOne.style.transitionDuration = `${0.75}s`;
-    outerTwo.style.opacity = 0;
-  }
-});
+  //burger menu pop-up
+  checkBox.addEventListener("click", () => {
+    if (menu.style.visibility === "visible") {
+      menu.style.visibility = "hidden";
+      outerOne.style.transform = "scale(0)";
+      outerOne.style.transitionDuration = `${0.75}s`;
+      outerTwo.style.opacity = 0;
+    } else {
+      menu.style.visibility = "visible";
+      outerOne.style.transform = "scale(1)";
+      outerOne.style.transitionDuration = `${0.75}s`;
+      outerTwo.style.opacity = 1;
+    }
+  });
 
-//close the menu once clicked on li items
-liItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    menu.style.visibility = "hidden";
-    outerOne.style.transform = "scale(0)";
-    outerOne.style.transitionDuration = `${0.75}s`;
-    outerTwo.style.opacity = 0;
+  //close the menu once clicked on li items
+  liItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      menu.style.visibility = "hidden";
+      outerOne.style.transform = "scale(0)";
+      outerOne.style.transitionDuration = `${0.75}s`;
+      outerTwo.style.opacity = 0;
+    });
   });
 });
-
-liItems;
